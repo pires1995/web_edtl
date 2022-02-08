4c37175f15204af783f098b7698cf72a8c58d3f6
pires1995 <55665833+pires1995@users.noreply.github.com>
2022-02-08 15:08:01 +0000

    update

--- web_edtl/web_edtl/settings.py
+++ web_edtl/web_edtl/settings.py
@@ -102,10 +102,10 @@ WSGI_APPLICATION = 'web_edtl.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'web_edtl_2',
-        'USER': 'root',
-        'PASSWORD': 'Timor2021',
-        'HOST': 'localhost',
+        'NAME': 'web_edtl',
+        'USER': 'admin',
+        'PASSWORD': 'Leste_123',
+        'HOST': 'database-kinos.cx862xedog9t.ap-southeast-1.rds.amazonaws.com',
         'PORT': '3306',
     }
 }
